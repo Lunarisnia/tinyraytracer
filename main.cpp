67cf35de41fac7f8dc90a1f3308102e4a52e9c5b
louna <r.arswendo.r@gmail.com>
2024-10-20 19:52:25 +0700

    wip: rays

--- main.cpp
+++ main.cpp
@@ -3,15 +3,31 @@
 #include <iostream>
 #include <vector>
 
-void render() {
+struct Sphere {
+  Vec3f center;
+  float radius;
+
+  Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+};
+
+Vec3f cast_ray(const Vec3f &origin, const Vec3f &direction,
+               const Sphere &sphere) {
+  return Vec3f(0.4, 0.2, 0.1);
+}
+
+void render(const Sphere &sphere) {
   const int width = 1024;
   const int height = 768;
+  const int fov = M_PI / 2.0;
   std::vector<Vec3f> framebuffer(width * height);
 
   for (size_t j = 0; j < height; j++) {
     for (size_t i = 0; i < width; i++) {
-      framebuffer[i + j * width] =
-          Vec3f(j / float(height), i / float(width), 0);
+      float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) * width /
+                (float)height;
+      float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
+      Vec3f dir = Vec3f(x, y, -1).normalize();
+      framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, sphere);
     }
   }
 
@@ -27,6 +43,7 @@ void render() {
 }
 
 int main() {
-  render();
+  Sphere sphere(Vec3f(-13, 20, -2), 20);
+  render(sphere);
   return 0;
 }
